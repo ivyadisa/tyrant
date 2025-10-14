c75a052661e242bf906b125fe973481c74e56ad4
ivyadisa <ivyadisa7@gmail.com>
2025-10-14 15:02:59 +0300

    Add user profile and update user profile

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView, CustomLoginView, UserListView
+from .views import RegisterView, CustomLoginView, UserListView,  user_profile,  update_user_profile
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('list/', UserListView.as_view(), name='user-list'),
+    path('profile/', user_profile, name='user-profile'),
+    path('update/', update_user_profile, name='update-user-profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 # users/views.py
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions 
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from .models import User
 from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 # Register new user
 class RegisterView(generics.CreateAPIView):
@@ -31,3 +34,22 @@ class CustomLoginView(ObtainAuthToken):
             'role': token.user.role,
             'status': token.user.status,
         })
+
+#View user profile
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user)
+    return Response(serializer.data)
+
+# update user profile
+@api_view(['PUT', 'PATCH'])
+@permission_classes([IsAuthenticated])
+def update_user_profile(request):
+    user = request.user
+    serializer = UserSerializer(user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
