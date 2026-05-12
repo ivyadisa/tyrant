09f189dbf53e4c05f0da0fb5f5923e73e3238f1e
Moses <moseswamboga4@gmail.com>
2026-05-12 16:17:22 +0300

    updated the timeout

--- tyrent_backend/settings.py
+++ tyrent_backend/settings.py
@@ -267,8 +267,8 @@ else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = os.getenv("EMAIL_HOST", "mail.spaceship.com")
     EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-    EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = False
+    EMAIL_USE_TLS = False
+    EMAIL_USE_SSL = True
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
     EMAIL_TIMEOUT = 30
